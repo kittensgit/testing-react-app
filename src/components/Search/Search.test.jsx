3c5569336ebc89d0a3fd8a850d046d676f1afda3
Nika <nikaonime@gmail.com>
2023-07-06 13:51:57 +0300

    getByPlaceholderText, jest.fn(), userEvent.type()

--- src/components/Search/Search.test.jsx
+++ src/components/Search/Search.test.jsx
@@ -1,6 +1,7 @@
 import { screen, render } from "@testing-library/react";
 
 import Search from "./Search";
+import userEvent from "@testing-library/user-event";
 
 const onChange = jest.fn()
 
@@ -13,5 +14,26 @@ describe('Search component', () => {
         )
 
         expect(screen.getByText(/find/i)).toBeInTheDocument()
+    })
+    it('Search without children', () => {
+        render(<Search value='' onChange={onChange} />)
+        expect(screen.getByText(/search/i)).toBeInTheDocument()
+    })
+    it('Search without placeholder', () => {
+        render(<Search value='' onChange={onChange} />)
+        expect(screen.getByPlaceholderText(/search.../i)).toBeInTheDocument()
+    })
+    it('Custom placeholder works correctly', () => {
+        render(<Search value='' onChange={onChange} placeholder={'find post'} />)
+        expect(screen.getByPlaceholderText(/find post/i)).toBeInTheDocument()
+    })
+    it('onChange works', () => {
+        render(
+            <Search value='' onChange={onChange}>
+                Find: 
+            </Search>
+        )
+        userEvent.type(screen.getByRole('textbox'), 'React')
+        expect(onChange).toHaveBeenCalledTimes(5)
     })
 })
